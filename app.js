1c48d388975417c67907549fd3f42b207158e1a8
Minh Anh <daylaminhanhne555@gmail.com>
2026-07-04 10:27:39 +0700

    feat: add Joi validation for spots

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const {spotSchema} = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/expressError');
 const methodOverride = require('method-override');
 const Spot = require('./models/spot');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://127.0.0.1:27017/travelSpot')
     .then(() => {
@@ -30,6 +34,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateSpot = (req, res, next) => {
+    const { error } = spotSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+    console.log(result);
+}
+
 app.get('/', (req, res) => {
     res.render('home')
 });
@@ -43,7 +58,8 @@ app.get('/spots/new', (req, res) => {
     res.render('spots/new');
 });
 
-app.post('/spots', catchAsync(async (req, res) => {
+app.post('/spots', validateSpot, catchAsync(async (req, res) => {
+    // if (!req.body.spot) throw new ExpressError('Invalid Spot Data', 400);
     const spot = new Spot(req.body.spot);
     await spot.save();
     res.redirect(`/spots/${spot._id}`)
@@ -59,7 +75,7 @@ app.get('/spots/:id/edit', catchAsync(async (req, res) => {
     res.render('spots/edit', { spot })
 }));
 
-app.put('/spots/:id', catchAsync(async (req, res) => {
+app.put('/spots/:id',validateSpot, catchAsync(async (req, res) => {
     const { id } = req.params;
     const spot = await Spot.findByIdAndUpdate(id, { ...req.body.spot })
     res.redirect(`/spots/${spot._id}`)
@@ -71,8 +87,27 @@ app.delete('/spots/:id', catchAsync(async (req, res) => {
     res.redirect('/spots');
 }));
 
+//review
+// app.post('/spots/:id/reviews', catchAsync(async(req,res) => {
+//     const spot = await Spot.findById(req.params.id);
+//     const review = new Review(req.body.review);
+//     spot.reviews.push(review);
+//     await review.save();
+//     await spot.save();
+//     res.redirect(`/spots/${spot._id}`);
+// }))
+
+
+app.all('/{*path}', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
 app.use((err, req, res, next) => {
-    res.send('Oh no, something went wrong')
+    const { statusCode = 500 } = err;
+    if (!err.message) {
+        err.message = 'Oh no, Something went wrong!';
+    }
+    res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
