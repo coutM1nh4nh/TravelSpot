6bcd58ef4f74ef65b75584edcf9e9e58bd666f20
Minh Anh <daylaminhanhne555@gmail.com>
2026-07-08 21:53:31 +0700

    fix: register route

--- routes/users.js
+++ routes/users.js
@@ -12,20 +12,23 @@ router.post('/register', catchAsync(async (req, res) => {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success', 'Welcome to Travel Spot');
-        res.redirect('/spots');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Travel Spot');
+            res.redirect('/spots');
+        })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
     }
 }));
 
-router.get('/login', (req,res) => {
+router.get('/login', (req, res) => {
     res.render('users/login')
 });
 
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect:'/login'}) ,(req,res) => {
-    req.flash('success','Welcome back!');
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
     res.redirect('/spots');
 });
 
