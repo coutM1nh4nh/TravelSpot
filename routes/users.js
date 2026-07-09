fd86e788437088818be9dbc07a7df4a99dd41751
Minh Anh <daylaminhanhne555@gmail.com>
2026-07-09 21:31:38 +0700

    refactor: users routes

--- routes/users.js
+++ routes/users.js
@@ -2,59 +2,19 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
+const users = require('../controllers/users');
+
 const User = require('../models/user');
+
 const { storeReturnTo } = require('../middleware');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-})
-router.post('/register', catchAsync(async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Travel Spot');
-            res.redirect('/spots');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
+router.get('/register', users.renderRegister)
+router.post('/register', catchAsync(users.register));
 
-router.get('/login', (req, res) => {
-    res.render('users/login')
-});
+router.get('/login', users.renderLogin);
 
-router.post(
-    '/login',
-    storeReturnTo,
-    passport.authenticate('local', {
-        failureFlash: true,
-        failureRedirect: '/login'
-    }),
-    (req, res) => {
-        req.flash('success', 'Welcome back!');
+router.post('/login', storeReturnTo,passport.authenticate('local', {failureFlash: true,failureRedirect: '/login'}), users.login);
 
-        const redirectUrl = res.locals.returnTo || '/spots';
-
-        delete req.session.returnTo; 
-        
-        res.redirect(redirectUrl);
-    }
-);
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-
-        req.flash('success', 'Goodbye!');
-        res.redirect('/spots');
-    });
-});
+router.get('/logout', users.logout);
 
 module.exports = router;
