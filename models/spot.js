73a48246b2037fa4076543ecae3b230dc5b29ec5
Minh Anh <daylaminhanhne555@gmail.com>
2026-07-10 20:22:50 +0700

    feat: add thumbnail virtual property

--- models/spot.js
+++ models/spot.js
@@ -2,14 +2,19 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
-const spotSchema = new Schema ({
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload','/upload/w_200');
+})
+const spotSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],        
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -25,8 +30,8 @@ const spotSchema = new Schema ({
     ]
 });
 
-spotSchema.post('findOneAndDelete', async function(doc){
-    if(doc) {
+spotSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
         await Review.deleteMany({
             _id: {
                 $in: doc.reviews
